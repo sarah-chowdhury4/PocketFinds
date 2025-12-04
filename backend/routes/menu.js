5c954e059694bb9354f34b693136db122b1da12a
prantooshhh <sajidhossain7734@gmail.com>
2025-12-05 05:19:23 +0600

    Update menu.js
    fixed stall owner role requirement for creating items.
    
    added routing for update and delete items.
    
    added routing for getMenu to get all items of a stall

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import Stall from '../models/stall.model.js';
 import Item from '../models/item.model.js';
-import { createItem } from '../controllers/itemController.js';
+import { createItem, updateItem, deleteItem, getMenu } from '../controllers/itemController.js';
+import { requireAuth, requireRole } from '../middleware/auth.js';
 
 const router = express.Router();
 
-// POST "/addmenu" to add a menu
-router.post('/', createItem)
+// POST, PUT, DELETE routes for stall owners to manage menu items
+router.post('/', requireAuth, requireRole('stall owner'), createItem);
+router.put('/:id', requireAuth, requireRole('stall owner'), updateItem);
+router.delete('/:id', requireAuth, requireRole('stall owner'), deleteItem);
 
+// GET "/menu/:stall_id" to get menu by stall ID
+router.get('/:stall_id', getMenu);
 
 export default router;

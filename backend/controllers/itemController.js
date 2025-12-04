dce1083773f55c3cac394e2a6301e8d1645257c5
prantooshhh <sajidhossain7734@gmail.com>
2025-12-05 04:55:46 +0600

    Update itemController.js
    added updateItem, deleteItem and getMenu

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -13,6 +13,44 @@ const createItem = async (req, res) => {
     }
 
     // res.json({msg: "POST a new item"})
-}
+};
 
-export { createItem };
+const updateItem = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+
+        const item = await Item.findByIdAndUpdate(id, updates, { new: true});
+        if (!item) {
+            return res.status(404).json({ error: 'Item not found' });
+        }
+        return res.status(200).json(item);
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to update item' });
+    }
+};
+
+const deleteItem = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const item = await Item.findByIdAndDelete(id);
+        if (!item) {
+            return res.status(404).json({ error: 'Item not found' });
+        }
+        return res.status(200).json(item);
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to delete item' });
+    }
+};
+
+const getMenu = async (req, res) => {
+    try {
+        const { stall_id } = req.params;
+        const items = await Item.find({ stall_id });
+        return res.status(200).json(items);
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to fetch items' });
+    }
+};
+
+export { createItem, updateItem, deleteItem, getMenu };

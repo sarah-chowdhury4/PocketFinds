11e434fc7dcefe19b75bd72ad0a709b2ceb9d2ad
prantooshhh <sajidhossain7734@gmail.com>
2025-12-18 12:16:39 +0600

    Added profile edit/customization
    Added getUserProfile and updateUserProfile for profile edit/customize in controller\userController.js.
    Added necessary routes in routes\user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,19 @@
 import express from 'express';
 
 // controller functions import
-import { loginUser, signupUser } from '../controllers/userController.js';
+import { loginUser, signupUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { requireAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 // login route
 router.post('/login', loginUser);
 
-// logout route
+// signup route
 router.post('/signup', signupUser);
+
+// profile routes
+router.get('/profile', requireAuth, getUserProfile);
+router.put('/profile', requireAuth, updateUserProfile);
 
 export default router;

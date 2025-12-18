ac50a9d240d04502a49193259f3688cad203465e
prantooshhh <sajidhossain7734@gmail.com>
2025-12-18 12:20:01 +0600

    Revert "Added profile edit/customization"
    This reverts commit 11e434fc7dcefe19b75bd72ad0a709b2ceb9d2ad.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,19 +1,14 @@
 import express from 'express';
 
 // controller functions import
-import { loginUser, signupUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
-import { requireAuth } from '../middleware/auth.js';
+import { loginUser, signupUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 // login route
 router.post('/login', loginUser);
 
-// signup route
+// logout route
 router.post('/signup', signupUser);
-
-// profile routes
-router.get('/profile', requireAuth, getUserProfile);
-router.put('/profile', requireAuth, updateUserProfile);
 
 export default router;
